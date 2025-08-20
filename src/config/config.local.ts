7cfd50d1331fa87c72b284553169a6c0e53fa061
不就订个外卖么 <yeclimeric@gmail.com>
2025-08-20 13:43:26 +0800

    feat: 添加sqlite支持并修改数据库配置
    将默认数据库从mysql切换为sqlite，并添加相关依赖
    移除mysql配置，使用本地sqlite文件存储数据

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -1,6 +1,7 @@
 import { CoolConfig } from '@cool-midway/core';
 import { MidwayConfig } from '@midwayjs/core';
 import { TenantSubscriber } from '../modules/base/db/tenant';
+import * as path from 'path';
 
 /**
  * 本地开发 npm run dev 读取的配置文件
@@ -9,12 +10,12 @@ export default {
   typeorm: {
     dataSource: {
       default: {
-        type: 'mysql',
-        host: '127.0.0.1',
-        port: 3306,
-        username: 'root',
-        password: '123456',
-        database: 'cool',
+        type: 'sqlite',
+        // host: '127.0.0.1',
+        // port: 3306,
+        // username: 'root',
+        // password: '123456',
+        database: path.join(__dirname, '../../cool.sqlite'),
         // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
         synchronize: true,
         // 打印日志
